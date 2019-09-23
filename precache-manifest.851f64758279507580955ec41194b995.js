62418530db87cd0d568ec2f5c54de94aaea2dd9e
magrathea7 <kehl42rp@gmail.com>
2019-09-23 15:49:59 +0200

    Updates

---
+++ precache-manifest.851f64758279507580955ec41194b995.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40079fea0e38c781a451737d9c303a33",
+    "url": "/myreact/index.html"
+  },
+  {
+    "revision": "ff8cc15e2444b7d00e69",
+    "url": "/myreact/static/css/2.006be587.chunk.css"
+  },
+  {
+    "revision": "64e1746b61553d30572b",
+    "url": "/myreact/static/css/main.5a57f0e0.chunk.css"
+  },
+  {
+    "revision": "ff8cc15e2444b7d00e69",
+    "url": "/myreact/static/js/2.cf3989ea.chunk.js"
+  },
+  {
+    "revision": "64e1746b61553d30572b",
+    "url": "/myreact/static/js/main.a962f2d5.chunk.js"
+  },
+  {
+    "revision": "ccbb06cd3d33c4848531",
+    "url": "/myreact/static/js/runtime-main.379d2b37.js"
+  }
+]);
